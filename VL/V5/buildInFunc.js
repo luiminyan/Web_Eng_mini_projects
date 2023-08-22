469fe221ad68bcb409fc9083f507b4f85cbf04c9
Minh <94862685+luiminyan@users.noreply.github.com>
2023-08-22 11:28:04 +0200

    deleted:    VL/V5/ES6test.html 	new file:   VL/V5/buildInFunc.js 	deleted:    VL/V5/staticTest.html 	deleted:    VL/V5/tsconfig.json

---
+++ VL/V5/buildInFunc.js
@@ -0,0 +1,22 @@
+// Math
+// 去除小数部分
+console.log(Math.trunc(-42.7));
+// 
+console.log(Math.sign(20));
+console.log(Math.pow(2,4));
+console.log(Math.PI);
+
+
+// Number
+// 最小的数字
+console.log(Number.MAX_VALUE);
+// 最大的数字
+console.log(Number.MIN_VALUE);
+var str01 = 'Hello world!';
+// is not a number
+console.log(isNaN(str01));
+console.log(typeof(str01));
+
+// Array
+const numbers = [8, 2, 7, 4, 0.5];
+console.log(typeof(numbers[0]));
