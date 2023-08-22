a492b889e17b98045931ff3de7fe2e1ba0408a52
Minh <94862685+luiminyan@users.noreply.github.com>
2023-08-22 11:36:33 +0200

    modified:   VL/V5/buildInFunc.js

--- VL/V5/buildInFunc.js
+++ VL/V5/buildInFunc.js
@@ -18,5 +18,22 @@ console.log(isNaN(str01));
 console.log(typeof(str01));
 
 // Array
-const numbers = [8, 2, 7, 4, 0.5];
+const numbers = [8, 2, 7, 4, 15];
 console.log(typeof(numbers[0]));
+
+console.log(numbers.sort());
+
+console.log(numbers);
+
+console.log(numbers.slice(1, 4));
+
+console.log(numbers.find(x => x > 10));
+
+console.log(numbers.findIndex(x => x > 10));
+
+// String
+console.log(" A B C ".trim());
+console.log("abcdefg".toUpperCase());
+console.log("abcdef".indexOf("c"));
+console.log("abcdef".charAt(1));
+console.log("Hello".includes("ello"));
