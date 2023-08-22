57cad4231719b5f369352b628c13d1dd2d98e345
Minh <94862685+luiminyan@users.noreply.github.com>
2023-08-22 15:03:21 +0200

    modified:   VL/V5/V5_2023_TS_16_9.pdf 	new file:   VL/V5/typeScript.html 	new file:   VL/V5/typeScript.js 	modified:   VL/V5/typeScript.ts

---
+++ VL/V5/typeScript.js
@@ -0,0 +1,52 @@
+function sell(obj) {
+    if (!(obj.inspected)) {
+        console.log("Werkstatt aufsuchen.");
+    }
+    else {
+        console.log("".concat(obj.name, ", Baujahr: ").concat(obj.year, ", im Top Zustand! "));
+    }
+}
+sell({ name: "Ford", year: 2002, inspected: false });
+sell({ name: "Ford KA", inspected: true });
+var Firma = /** @class */ (function () {
+    function Firma(p) {
+        this.personal = p;
+    }
+    // function
+    Firma.prototype.sortByName = function () {
+        // make a copy of the personal array
+        var result = this.personal.slice(0);
+        // alternative: var result = [...this.personal];
+        // console.log(result);
+        // 给sort添加比较function （arrow func.）
+        result.sort(function (x, y) { return x.name.localeCompare(y.name); });
+        return result;
+    };
+    return Firma;
+}());
+var alex = { name: 'Alex', age: 45 };
+var maria = { name: 'Maria', age: 42 };
+var tabea = { name: 'Tabea', age: 24 };
+var maik = { name: 'Maik', age: 32 };
+var employeesList = [alex, maria, tabea, maik];
+var company01 = new Firma(employeesList);
+console.log(company01.sortByName());
+function loadTable(firma) {
+    var tableBody = document.getElementById('tableData');
+    var dataHTML = '';
+    // key = index, value = name ... age ...
+    for (var _i = 0, _a = Object.entries(firma.personal); _i < _a.length; _i++) {
+        var _b = _a[_i], key = _b[0], value = _b[1];
+        dataHTML += "<tr> <th> ".concat(value.name, " </th>");
+        dataHTML += " <th> ".concat(value.age, " </th> </tr>");
+    }
+    var loadBtn = document.getElementById('button');
+    // !!!! ATTENTION :check not null of the DOM-elements
+    if (loadBtn != null && tableBody != null) {
+        loadBtn.addEventListener('click', function () {
+            // reset the innerHTML of the DOM-element
+            tableBody.innerHTML = dataHTML;
+        });
+    }
+}
+loadTable(company01);
--- VL/V5/typeScript.ts
+++ VL/V5/typeScript.ts
@@ -28,4 +28,50 @@ class Firma {
     constructor (p : Person[]) {
         this.personal = p;
     }
+
+    // function
+    sortByName() {
+        // make a copy of the personal array
+        var result = this.personal.slice(0);
+        // alternative: var result = [...this.personal];
+
+        // console.log(result);
+        // 给sort添加比较function （arrow func.）
+        result.sort((x, y) => {return x.name.localeCompare(y.name)});
+        return result;
+    }
 }
+
+
+var alex: Person = { name: 'Alex', age: 45 }; 
+var maria: Person = { name: 'Maria', age: 42 };
+var tabea : Person = { name: 'Tabea', age: 24 };
+var maik : Person = { name: 'Maik', age: 32 };
+
+var employeesList : Person[] = [alex, maria, tabea, maik];
+var company01 = new Firma(employeesList);
+
+
+console.log(company01.sortByName());
+
+
+function loadTable (firma : Firma) {
+    var tableBody = document.getElementById('tableData');
+    var dataHTML = '';
+    // key = index, value = name ... age ...
+    for (let [key, value] of Object.entries(firma.personal)) {
+        dataHTML += `<tr> <th> ${value.name} </th>`;
+        dataHTML += ` <th> ${value.age} </th> </tr>`;
+    }
+
+    var loadBtn = document.getElementById('button');
+    // !!!! ATTENTION :check not null of the DOM-elements
+    if (loadBtn != null && tableBody != null) {
+        loadBtn.addEventListener('click', () => {
+            // reset the innerHTML of the DOM-element
+            tableBody.innerHTML = dataHTML;
+        });
+    }
+}
+
+loadTable(company01);
